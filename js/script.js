12fa98d5ef93878e1ca4029e95b2fcc814978e44
Laura Vitali <lauravitali@MacBook-Pro-di-Laura.local>
2020-09-02 12:25:06 +0200

    media queries

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//fetch data
+fetch("https://kea-alt-del.dk/t5/api/productlist")
+    .then(function (res) {
+        console.log(res);
+        return res.json();
+    })
+    .then(function (data){
+    console.log(data);
+})
+
+//loop through products
+
+//find template
+
+//clone template
+
+//fill in the template
+
+//append
